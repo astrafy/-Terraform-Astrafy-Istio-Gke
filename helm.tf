86b10e72ca7bb4b016d6db7da159e6c49048f578
kevin-astrafy <kevin@astrafy.io>
2022-07-17 12:54:21 +0200

    adding istiod
    Signed-off-by: kevin-astrafy <kevin@astrafy.io>

--- helm.tf
+++ helm.tf
@@ -7,5 +7,13 @@ resource "helm_release" "istio_base" {
   version    = "1.14.1"
   namespace  = kubernetes_namespace.istio_system.metadata.0.name
 
+}
+
+resource "helm_release" "istio_discovery" {
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+  version    = "1.14.1"
+  namespace  = kubernetes_namespace.istio_system.metadata.0.name
 
 }
