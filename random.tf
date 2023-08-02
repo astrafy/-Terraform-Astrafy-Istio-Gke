70d4f87f76dd6796003c4f7a65e2f5f88c46261b
Alex de la Cruz <46356295+acrulopez@users.noreply.github.com>
2023-08-02 09:07:52 +0200

    one certificate per host

--- random.tf
+++ random.tf
@@ -1,6 +1,7 @@
 resource "random_id" "istio_ingress_lb_certificate" {
+  for_each    = [for host in var.istio_ingress_configuration.hosts : host.host]
   byte_length = 4
-  prefix      = "istio-ingress-lb-cert"
+  prefix      = "istio-ingress-lb-cert-${each.value}"
   keepers = {
     domains = join(",", [for host in var.istio_ingress_configuration.hosts : host.host])
   }
