ba75838fbb0a180a434a986f71c2c604b39152eb
Alex de la Cruz <46356295+acrulopez@users.noreply.github.com>
2023-08-21 13:38:28 +0200

    fix: one certificate per host recreating because of random_id keeper

--- random.tf
+++ random.tf
@@ -2,7 +2,4 @@ resource "random_id" "istio_ingress_lb_certificate" {
   for_each    = toset([for host in var.istio_ingress_configuration.hosts : host.host])
   byte_length = 4
   prefix      = "istio-ingress-lb-cert-${each.value}"
-  keepers = {
-    domains = join(",", [for host in var.istio_ingress_configuration.hosts : host.host])
-  }
 }
